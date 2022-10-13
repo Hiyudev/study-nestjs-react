ac714ef3a4ce660b9e2b188caa44e7bac1a1e165
Hiyudev <37001732+Hiyudev@users.noreply.github.com>
2022-10-13 12:44:26 -0300

    feat: add basic graphql fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { gql, useQuery } from "@apollo/client";
+
+const GET_USERS = gql`
+  query Query {
+    users {
+      email
+      username
+      institutionAssociated {
+        name
+        abreviation
+      }
+    }
+  }
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { loading, data } = useQuery(GET_USERS);
+
+  if (loading) {
+    return (
+      <div className="bg-gray-900 text-white min-h-screen flex justify-center items-center">
+        Loading...
+      </div>
+    );
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="bg-gray-900 text-white min-h-screen flex justify-center items-center p-4">
+      <ul className="bg-gray-800 p-4 rounded-md w-full md:w-1/2">
+        {data.users.map((user: any) => {
+          return (
+            <li className="py-2">
+              <div className="flex flex-row gap-2 items-center pb-2">
+                <h1>{user.username}</h1>
+                {user.email && (
+                  <span className="text-gray-300">{user.email}</span>
+                )}
+              </div>
+              <hr className="border-gray-600" />
+
+              {user.institutionAssociated && (
+                <small className="flex justify-between">
+                  <span>{user.institutionAssociated.name}</span>
+                  <span>{user.institutionAssociated.abreviation}</span>
+                </small>
+              )}
+            </li>
+          );
+        })}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
